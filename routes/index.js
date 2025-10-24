c0ffa5cbced876253f66a9c325ffc06cbc342b3f
Pola Khalil <50252495+PolaKhalil@users.noreply.github.com>
2025-10-24 16:13:05 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+router.get('/new_account', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/new_account.html'));
+});
+
 module.exports = router;
