a991a2514b0bea107c90920ffc1f16aa2020498c
PolaKhalil <polakhalil.hi@gmail.com>
2025-10-24 15:28:20 +0300

    update5

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
+// Serve static files from "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve static files from the "assets" directory
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-// Use the router for handling routes
+// Router
 app.use('/', indexRouter);
 
-// Catch-all route for handling 404 errors
+// 404 handler
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
